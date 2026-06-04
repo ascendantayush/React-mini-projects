8a23740f2c991edf75930e02e6557336ef22b475
Ayush Raj <ayush.raj2023a@vitstudent.ac.in>
2026-06-04 18:10:18 +0530

    added the api for country

--- country-flags/src/App.jsx
+++ country-flags/src/App.jsx
@@ -1,37 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "./styles/App.module.scss";
 import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
 import Dropdown from "./components/Dropdown.jsx";
 import Card from "./components/Card.jsx";
 
+let key = 0;
 const App = () => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://restcountries.com/v3.1/all?fields=name,population,flags,region,capital",
+    )
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
+
   return (
     <div className={style["main-container"]}>
-      <Header></Header>
+      <Header />
       <div className={style["inner-container"]}>
-        <SearchBar></SearchBar>
-        <Dropdown></Dropdown>
+        <SearchBar />
+        <Dropdown />
       </div>
-
       <div className={style["content"]}>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
+        {
+          //  { flag, name, population, region, capital }
+          countries.map((data) => {
+            return (
+              <Card
+                key={key++}
+                flag={data.flags.svg}
+                name={data.name.common}
+                population={data.population}
+                region={data.region}
+                capital={data.capital?.[0]}
+              ></Card>
+            );
+          })
+        }
       </div>
     </div>
   );
--- country-flags/src/components/Card.jsx
+++ country-flags/src/components/Card.jsx
@@ -1,30 +1,28 @@
 import React from "react";
 import style from "./Card.module.scss";
 
-const Card = () => {
+const Card = ({ flag, name, population, region, capital }) => {
   return (
     <>
       <div className={style.cardContainer}>
-        <img
-          className={style.cardImage}
-          src="https://flagcdn.com/ai.svg"
-          alt="country-flag"
-        />
+        <img className={style.cardImage} src={flag} alt="country-flag" />
 
         <div className={style.information}>
-          <h2 className={style.countryName}>Afghanistan</h2>
+          <h2 className={style.countryName}>{name}</h2>
 
           <div className={style.countryStats}>
             <p className={style.countryDetail}>
-              <b>Population:</b> 40218234 people
+              <b>Population:</b>
+              {population} people
             </p>
 
             <p className={style.countryDetail}>
-              <b>Region:</b> Asia
+              <b>Region:</b>
+              {region}
             </p>
 
             <p className={style.countryDetail}>
-              <b>Capital:</b> Kabul
+              <b>Capital:</b> {capital}
             </p>
           </div>
         </div>
