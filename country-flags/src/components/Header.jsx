fe053c730bfce49c07d4ec9ffbc91ff68a21bccc
Ayush Raj <ayush.raj2023a@vitstudent.ac.in>
2026-06-06 18:25:21 +0530

    added the click back thome on title

--- country-flags/src/components/Header.jsx
+++ country-flags/src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import style from "./Header.module.scss";
-
+import { Link } from "react-router-dom";
 const icon = new URL("../assets/moon-solid-full.svg", import.meta.url).href;
 
 const Header = () => {
   return (
     <div className={style.header}>
-      <h2 className={style.headerTitle}>Where in the world?</h2>
+      <Link to={`/`} className={style.LinkHeader}>
+        <h2 className={style.headerTitle}>Where in the world?</h2>
+      </Link>
       <button className={style.headerBtn}>
         <img className={style.headerIcon} src={icon} alt="icon" />
         <span className={style.headerText}>Dark Mode</span>
